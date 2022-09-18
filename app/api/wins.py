895d7194dc5f99aa49864ad183a29c28019c1582
Andrew Hynes <andrewjhynes@gmail.com>
2022-09-18 00:40:54 -0230

    feat: get wins including supremacies

--- app/api/wins.py
+++ app/api/wins.py
@@ -1,11 +1,14 @@
 from fastapi import Depends
 from fastapi.routing import APIRouter
 from sqlalchemy.dialects import postgresql
+from sqlalchemy.sql.functions import coalesce
 from sqlmodel import Session, select
 
 from app.api.util import get_session
 from app.models.game import Game
+from app.models.military_supremacy import MilitarySupremacy
 from app.models.player import Player
+from app.models.scientific_supremacy import ScientificSupremacy
 from app.models.score import Score
 
 router = APIRouter()
@@ -17,7 +20,7 @@ def get_wins(
     session: Session = Depends(get_session),
 ):
 
-    total_score_cte = select(
+    total_scores = select(
         Score.game_id,
         Score.player_id,
         (
@@ -32,16 +35,37 @@ def get_wins(
         ).label("total"),
     ).cte()
 
+    score_winners = (
+        select(total_scores.c.game_id, total_scores.c.player_id)
+        .distinct(total_scores.c.game_id)
+        .order_by(total_scores.c.game_id, total_scores.c.total.desc())
+    ).cte()
+
+    game_winners = (
+        select(
+            Game.id.label("game_id"),
+            coalesce(
+                score_winners.c.player_id,
+                MilitarySupremacy.player_id,
+                ScientificSupremacy.player_id,
+            ).label("player_id"),
+        )
+        .distinct(Game.id)
+        .outerjoin(score_winners, Game.id == score_winners.c.game_id)
+        .outerjoin(MilitarySupremacy, Game.id == MilitarySupremacy.game_id)
+        .outerjoin(ScientificSupremacy, Game.id == ScientificSupremacy.game_id)
+        .order_by(Game.id)
+    ).cte()
+
     statement = (
         select(
-            total_score_cte.c.game_id,
+            game_winners.c.game_id,
             Game.date.label("game_date"),
             Player.name.label("winner"),
         )
-        .distinct(Game.id)
-        .join(Game, total_score_cte.c.game_id == Game.id)
-        .join(Player, total_score_cte.c.player_id == Player.id)
-        .order_by(Game.id, total_score_cte.c.total.desc())
+        .join(Player, game_winners.c.player_id == Player.id)
+        .join(Game, game_winners.c.game_id == Game.id)
+        .order_by(Game.id)
     )
 
     if game_id:
@@ -49,6 +73,4 @@ def get_wins(
 
     statement.compile(dialect=postgresql.dialect())
 
-    winners = session.exec(statement).all()
-
-    return winners
+    return session.exec(statement).all()
--- tests/api/test_wins.py
+++ tests/api/test_wins.py
@@ -1,8 +1,11 @@
 import pytest
 from fastapi.testclient import TestClient
+from sqlmodel import Session
 
 from app.models.game import Game
+from app.models.military_supremacy import MilitarySupremacy
 from app.models.player import Player
+from app.models.scientific_supremacy import ScientificSupremacy
 
 
 @pytest.mark.usefixtures("scores")
@@ -16,6 +19,40 @@ def test_get_wins(client: TestClient, games: list[Game], players: list[Player]):
         {"game_id": games[0].id, "game_date": games[0].date, "winner": players[1].name},
         {"game_id": games[1].id, "game_date": games[1].date, "winner": players[0].name},
         {"game_id": games[2].id, "game_date": games[2].date, "winner": players[0].name},
+    ]
+
+
+@pytest.mark.usefixtures("scores")
+def test_get_wins_with_supremacies(
+    session: Session, client: TestClient, games: list[Game], players: list[Player]
+):
+
+    for date in ["2022-09-17", "2022-09-18"]:
+        game = Game(date=date)
+        games.append(game)
+
+        session.add(game)
+
+    session.commit()
+
+    military_supremacy = MilitarySupremacy(game_id=games[3].id, player_id=players[0].id)
+    scientific_supremacy = ScientificSupremacy(
+        game_id=games[4].id, player_id=players[1].id
+    )
+
+    session.add_all([military_supremacy, scientific_supremacy])
+    session.commit()
+
+    response = client.get("/wins")
+
+    assert response.status_code == 200
+
+    assert response.json() == [
+        {"game_id": games[0].id, "game_date": games[0].date, "winner": players[1].name},
+        {"game_id": games[1].id, "game_date": games[1].date, "winner": players[0].name},
+        {"game_id": games[2].id, "game_date": games[2].date, "winner": players[0].name},
+        {"game_id": games[3].id, "game_date": games[3].date, "winner": players[0].name},
+        {"game_id": games[4].id, "game_date": games[4].date, "winner": players[1].name},
     ]
 
 
