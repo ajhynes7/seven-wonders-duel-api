4eb90b0e7d34e3860f416be441c1071a13d50d51
Andrew Hynes <andrew.hynes@colabsoftware.com>
2024-05-28 22:53:21 -0230

    test: combine win and total win testss

--- tests/api/test_wins.py
+++ tests/api/test_wins.py
@@ -9,11 +9,10 @@ from app.models.supremacy import Supremacy
 
 
 @pytest.mark.usefixtures("scores")
-def test_get_wins(client: TestClient, games: list[Game], players: list[Player]):
+def test_get_wins(client: TestClient, games: list[Game]):
     response = client.get("/wins")
 
     assert response.status_code == 200
-
     assert response.json() == [
         {
             "game_id": games[0].id,
@@ -27,6 +26,71 @@ def test_get_wins(client: TestClient, games: list[Game], players: list[Player]):
             "game_id": games[2].id,
             "player_id": 1,
         },
+    ]
+
+    response_total = client.get("/wins/total")
+
+    assert response_total.status_code == 200
+    assert response_total.json() == [
+        {"player_id": 1, "count": 2},
+        {"player_id": 2, "count": 1},
+    ]
+
+
+@pytest.mark.usefixtures("scores")
+def test_get_wins_with_tie(session: Session, client: TestClient, games: list[Game]):
+    game = Game(date="2024-05-28")
+    games.append(game)
+
+    session.add(game)
+    session.commit()
+
+    for player_id in [1, 2]:
+        score = Score(
+            game_id=game.id,
+            player_id=player_id,
+            civilian=1,
+            science=1,
+            commerce=1,
+            guilds=1,
+            wonders=1,
+            tokens=1,
+            coins=1,
+            military=1,
+        )
+        session.add(score)
+
+    session.commit()
+
+    response = client.get("/wins")
+
+    assert response.status_code == 200
+    assert response.json() == [
+        {
+            "game_id": games[0].id,
+            "player_id": 2,
+        },
+        {
+            "game_id": games[1].id,
+            "player_id": 1,
+        },
+        {
+            "game_id": games[2].id,
+            "player_id": 1,
+        },
+        {
+            "game_id": games[3].id,
+            "player_id": 0,
+        },
+    ]
+
+    response_total = client.get("/wins/total")
+
+    assert response_total.status_code == 200
+    assert response_total.json() == [
+        {"player_id": 0, "count": 1},
+        {"player_id": 1, "count": 2},
+        {"player_id": 2, "count": 1},
     ]
 
 
@@ -55,7 +119,6 @@ def test_get_wins_with_supremacies(
     response = client.get("/wins")
 
     assert response.status_code == 200
-
     assert response.json() == [
         {
             "game_id": games[0].id,
@@ -79,62 +142,23 @@ def test_get_wins_with_supremacies(
         },
     ]
 
+    response_total = client.get("/wins/total")
+
+    assert response_total.status_code == 200
+    assert response_total.json() == [
+        {"player_id": 1, "count": 3},
+        {"player_id": 2, "count": 2},
+    ]
+
 
 @pytest.mark.usefixtures("scores")
 def test_get_win_of_game(client: TestClient, games: list[Game]):
     response = client.get("/wins", params={"game_id": 1})
 
     assert response.status_code == 200
-
     assert response.json() == [
         {
             "game_id": games[0].id,
             "player_id": 2,
         }
-    ]
-
-
-@pytest.mark.usefixtures("scores")
-def test_get_total_wins(client: TestClient):
-    response = client.get("/wins/total")
-
-    assert response.status_code == 200
-
-    assert response.json() == [
-        {"player_id": 1, "count": 2},
-        {"player_id": 2, "count": 1},
-    ]
-
-
-@pytest.mark.usefixtures("scores")
-def test_get_total_wins_with_tie(client: TestClient, session: Session):
-    game = Game(date="2024-05-28")
-    session.add(game)
-    session.commit()
-
-    for player_id in [1, 2]:
-        score = Score(
-            game_id=game.id,
-            player_id=player_id,
-            civilian=1,
-            science=1,
-            commerce=1,
-            guilds=1,
-            wonders=1,
-            tokens=1,
-            coins=1,
-            military=1,
-        )
-        session.add(score)
-
-    session.commit()
-
-    response = client.get("/wins/total")
-
-    assert response.status_code == 200
-
-    assert response.json() == [
-        {"player_id": 0, "count": 1},
-        {"player_id": 1, "count": 2},
-        {"player_id": 2, "count": 1},
     ]
